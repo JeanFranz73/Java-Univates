8a4c9561d4cba5729220aa150f430f6fd439d188
JeanFranz73 <franzjean37@gmail.com>
2021-03-30 19:11:27 -0300

    30/03/2021 - Add files

---
+++ src/br/univates/java/poo/pratica10/Alambique.java
@@ -0,0 +1,47 @@
+package br.univates.java.poo.pratica10;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Alambique {
+
+    private Reservatorio reservaCana;
+    private Reservatorio reservaCachaca;
+    private Reservatorio reservaIntermediario;
+
+    private int garrafasCheias;
+    private double tamanhoGarrafas;
+
+    public Alambique() {}
+
+    public Alambique(double limiteCana, double limiteCachaca, double limiteInterm, double limiteGarrafas) {
+        this.reservaCana = new Reservatorio(limiteCana);
+        this.reservaCachaca = new Reservatorio(limiteCachaca);
+        this.reservaIntermediario = new Reservatorio(limiteInterm);
+        this.tamanhoGarrafas = limiteGarrafas;
+        this.garrafasCheias = 0;
+    }
+
+    public boolean iniciaProducao() {
+        boolean result = false;
+        while (true) {
+            if (!reservaCana.removeQtde(1000) || !reservaCachaca.addQtde(400) || !reservaIntermediario.addQtde(200)) {
+                break;
+            }
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean encheGarrafas() {
+        boolean result = false;
+        while (reservaCachaca.getQuantidade() >= 750) {
+            garrafasCheias++;
+            reservaCachaca.setQuantidade(reservaCachaca.getQuantidade() - 750);
+            result = true;
+        }
+        return result;
+    }
+}
